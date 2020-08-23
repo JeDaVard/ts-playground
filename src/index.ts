81706b314353a7943289d388059631659b596e31
Davit Vardanyan <jedavard@gmail.com>
2020-08-23 12:23:32 +0400

    linked list

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
+import { CharCollection } from './CharCollection'
 
 const arrInt = [10, 3, -5, 0]
+const arrStr = 'Xxbadc'
 
 const numbersCollection = new NumbersCollection(arrInt)
+const charCollection = new CharCollection(arrStr)
 
 const sorter = new Sorter(numbersCollection)
+const charSorter = new Sorter(charCollection)
 sorter.sort()
+charSorter.sort()
 console.log(sorter.collection)
+console.log(charSorter.collection)
