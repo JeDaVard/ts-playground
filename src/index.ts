92ccf5dedb0bc7e75af8154507d8770baf90dce8
Davit Vardanyan <jedavard@gmail.com>
2020-08-26 15:22:46 +0400

    collections inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,22 @@
-import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharCollection } from './CharCollection'
 import { LinkedList } from './LinkedList'
 
 const arrInt = [10, 3, -5, 0]
-// const arrStr = 'Xxbadc'
-//
-// const numbersCollection = new NumbersCollection(arrInt)
-// const charCollection = new CharCollection(arrStr)
-//
-// const sorter = new Sorter(numbersCollection)
-// const charSorter = new Sorter(charCollection)
-//
-// const linkedList = new LinkedList()
-//
-// linkedList.add(100)
-// linkedList.add(-200)
-// linkedList.add(-9000)
-// linkedList.add(30)
-// linkedList.add(4000)
-//
-// const linkedSorter = new Sorter(linkedList)
-// linkedSorter.sort()
-//
-// linkedList.print()
-//
-// sorter.sort()
-// charSorter.sort()
-// console.log(sorter.collection)
-// console.log(charSorter.collection)
+const arrStr = 'Xxbadc'
 
 const numberCollection = new NumbersCollection(arrInt)
 
 numberCollection.sort()
 console.log(numberCollection)
+
+const linkedList = new LinkedList()
+
+linkedList.add(100)
+linkedList.add(-200)
+linkedList.add(-9000)
+linkedList.add(30)
+linkedList.add(4000)
+
+linkedList.sort()
+linkedList.print()
